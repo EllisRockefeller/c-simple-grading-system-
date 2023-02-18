9b2a15f978789412b79a6f356361e63d901c3323
Ellis Armah ayikwei(Ellis Rockefeller) <43081274+EllisRockefeller@users.noreply.github.com>
2023-02-18 01:08:21 +0000

    Add files via upload

---
+++ high level programing.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+
+ int main(){
+     cout<< "simple grading sysytem"<<endl<<endl;
+int marks;
+cout<< "enter a mark between 0 to 100."<<endl;
+cout<<"enter marks here ";  cin>>marks;
+
+ if(marks>=70 && marks<=100)
+        {cout<<"A"<<endl;}
+    else if(marks>=60 && marks<=69)
+        {cout<<"B"<<endl;}
+    else if(marks>=50 && marks<=59)
+        {cout<<"C"<<endl;}
+    else if(marks>=40 && marks<=49)
+        {cout<<"D"<<endl;}
+    else {
+        cout<<"failed!"<<endl;}
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+ }
+
+
+
---
+++ high level programming.c
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+ int main(){
+
+
+ int Marks ;
+ cout<<"input mark";
+ cin>> Marks;
+
+ switch (Marks){
+ case >=70 || <=100:
+    cout<<"day is monday";
+    break;
+     case 2:
+      cout<<"day is tueseday";
+      break;
+     case 3:
+      cout<<"day is wednesday";
+      break;
+     case 4:
+      cout<<"day is thursday";
+      break;
+     case 5:
+      cout<<"day is friday";
+      break;
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+ }
+
+
